06640ff739f6d411d43e02aaf177fde150eed0e0
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2016-05-10 16:58:10 -0400

    attempt a polynom implementation

---
+++ source/Polynom.ml
@@ -0,0 +1,170 @@
+(* Quentin Carbonneaux - 2016 *)
+
+module type Factor = sig
+  type t and poly
+  val of_var: Types.id -> t
+  val compare: t -> t -> int
+  val subst: Types.id -> poly -> t -> poly
+end
+
+module type Monom = sig
+  type t and factor and poly
+  val is_one: t -> bool
+  val compare: t -> t -> int
+  val fold: (factor -> int -> 'a -> 'a) -> t -> 'a -> 'a
+  val one: t
+  val lift: factor -> t
+  val of_var: Types.id -> t
+  val mul_factor: factor -> int -> t -> t
+  val mul: t -> t -> t
+  val subst: Types.id -> poly -> t -> poly
+end
+
+module type Poly = sig
+  type t and monom
+  val zero: unit -> t
+  val const: float -> t
+  val of_var: Types.id -> t
+  val compare: t -> t -> int
+  val fold: (monom -> float -> 'a -> 'a) -> t -> 'a -> 'a
+  val mul_monom: monom -> float -> t -> t
+  val add_scale: float -> t -> t -> t
+  val add: t -> t -> t
+  val mul: t -> t -> t
+  val pow: int -> t -> t
+  val subst: Types.id -> t -> t -> t
+end
+
+module MkFactor(Pol: Poly) = struct
+
+  type t =
+    | Var of Types.id
+    | Max of Pol.t
+
+  type poly = Pol.t
+
+  let of_var v = Var v
+
+  let compare a b =
+    match a, b with
+    | Var v1, Var v2 -> compare v1 v2
+    | Max p1, Max p2 -> Pol.compare p1 p2
+    | Var _, Max _ -> -1
+    | Max _, Var _ -> +1
+
+  let subst id p = function
+    | Var v when v = id -> p
+    | Max p' -> Pol.subst id p p'
+    | Var v' -> Pol.of_var v'
+
+end
+
+module MkMonom(Pol: Poly)(Fac: Factor with type poly = Pol.t) = struct
+
+  (* Monoms of a polynomial are maps from factors
+     to their power in the monomial.
+  *)
+
+  module M = Map.Make(Fac)
+  type t = int M.t and factor = Fac.t and poly = Pol.t
+
+  let is_one = M.is_empty
+  let compare = (M.compare compare: t -> t -> int)
+  let fold = M.fold
+  let one = M.empty
+  let lift factor = M.add factor 1 one
+  let of_var v = lift (Fac.of_var v)
+
+  let get_pow f m =
+    try M.find f m with Not_found -> 0
+
+  let mul_factor f e m =
+    M.add f (e + get_pow f m) m
+
+  let mul m m' =
+    if is_one m then m' else
+    if is_one m' then m else
+    fold mul_factor m' m
+
+  let subst id p m =
+    let p = Obj.magic p in
+    fold begin fun f e res ->
+      Pol.mul (Pol.pow e (Fac.subst id p f)) res
+    end m (Pol.const 1.)
+
+end
+
+module MkPoly(Mon: Monom) = struct
+
+  (* Polynomials are represented as maps from monomials
+     to their coefficient.
+  *)
+
+  module M = Map.Make(Mon)
+  type t = float M.t and monom = Mon.t
+
+  let zero = M.empty
+  let const k = M.add Mon.one k zero
+  let compare = (M.compare compare: t -> t -> int)
+  let fold = M.fold
+  let of_var v = M.add (Mon.of_var v) 1. zero
+
+  let get_coeff m pol =
+    try M.find m pol with Not_found -> 0.
+
+  let mul_factor f e pol =
+    fold begin fun m coeff res ->
+      M.add (Mon.mul_factor f e m) coeff res
+    end pol zero
+
+  let mul_monom m coeff pol =
+    fold begin fun m' coeff' res ->
+      M.add (Mon.mul m m') (coeff *. coeff') res
+    end pol zero
+
+  let add_scale scale =
+    let f = function Some c -> c | None -> 0. in
+    M.merge (fun _ ac bc -> Some (scale *. f ac +. f bc))
+
+  let add = add_scale 1.
+
+  let mul =
+    fold begin fun m coeff res ->
+      add (mul_monom m coeff res) res
+    end
+
+  let rec pow n pol =
+    if n < 0 then failwith "invalid argument" else
+    if n = 0 then const 1. else
+    mul pol (pow (n-1) pol)
+
+  let subst id p p' =
+    let p = Obj.magic p in
+    fold begin fun m k res ->
+
+      (* Because we will always be using this module in
+         the recursive definition below, the Obj.magic
+         is safe.  [Mon.poly] is the same type as [t]
+         at runtime.
+      *)
+      add_scale k (Obj.magic (Mon.subst id p m)) res
+
+    end p' zero
+
+  let zero () = zero
+
+end
+
+module
+  rec Poly
+  : Poly with type monom = Monom.t
+  = MkPoly(Monom)
+
+  and Monom
+  : Monom with type factor = Factor.t
+    and type poly = Poly.t
+  = MkMonom(Poly)(Factor)
+
+  and Factor
+  : Factor with type poly = Poly.t
+  = MkFactor(Poly)
