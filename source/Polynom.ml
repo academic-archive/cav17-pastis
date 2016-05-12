cfcbc7a6c03f313cab81bdb0ce26fa15636cd561
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2016-05-12 09:57:13 -0400

    extract substitution from Mk modules

--- source/Polynom.ml
+++ source/Polynom.ml
@@ -1,13 +1,13 @@
 (* Quentin Carbonneaux - 2016 *)
 
 module type Factor = sig
-  type t and poly
+  type poly
+  type t = Var of Types.id | Max of poly
   val compare: t -> t -> int
-  val subst: Types.id -> poly -> t -> poly
 end
 
 module type Monom = sig
-  type t and factor and poly
+  type t and factor
   val is_one: t -> bool
   val compare: t -> t -> int
   val fold: (factor -> int -> 'a -> 'a) -> t -> 'a -> 'a
@@ -15,14 +15,13 @@ module type Monom = sig
   val of_factor: factor -> t
   val mul_factor: factor -> int -> t -> t
   val mul: t -> t -> t
-  val subst: Types.id -> poly -> t -> poly
 end
 
 module type Poly = sig
-  type t and factor and monom
+  type t and monom
   val zero: unit -> t
   val const: float -> t
-  val of_factor: factor -> t
+  val of_monom: monom -> t
   val compare: t -> t -> int
   val fold: (monom -> float -> 'a -> 'a) -> t -> 'a -> 'a
   val mul_monom: monom -> float -> t -> t
@@ -30,7 +29,6 @@ module type Poly = sig
   val add: t -> t -> t
   val mul: t -> t -> t
   val pow: int -> t -> t
-  val subst: Types.id -> t -> t -> t
 end
 
 (* we have to tie Poly.factor and Factor.t,
@@ -41,13 +39,10 @@ module MkFactor(Pol: Poly)
 : Factor with type poly = Pol.t
 = struct
 
+  type poly = Pol.t
   type t =
     | Var of Types.id
-    | Max of Pol.t
-
-  type poly = Pol.t
-
-  let of_var v = Var v
+    | Max of poly
 
   let compare a b =
     match a, b with
@@ -56,18 +51,10 @@ module MkFactor(Pol: Poly)
     | Var _, Max _ -> -1
     | Max _, Var _ -> +1
 
-  let pol_of_factor: t -> Pol.t
-  = fun x -> Pol.of_factor (Obj.magic x)
-
-  let subst id p = function
-    | Var v when v = id -> p
-    | Max p' -> pol_of_factor (Max (Pol.subst id p p'))
-    | f -> pol_of_factor f
-
 end
 
-module MkMonom(Pol: Poly)(Fac: Factor with type poly = Pol.t)
-: Monom with type factor = Fac.t and type poly = Pol.t
+module MkMonom(Fac: Factor)
+: Monom with type factor = Fac.t
 = struct
 
   (* Monoms of a polynomial are maps from factors
@@ -75,7 +62,8 @@ module MkMonom(Pol: Poly)(Fac: Factor with type poly = Pol.t)
   *)
 
   module M = Map.Make(Fac)
-  type t = int M.t and factor = Fac.t and poly = Pol.t
+  type factor = Fac.t
+  type t = int M.t
 
   let is_one = M.is_empty
   let compare = (M.compare compare: t -> t -> int)
@@ -94,15 +82,10 @@ module MkMonom(Pol: Poly)(Fac: Factor with type poly = Pol.t)
     if is_one m' then m else
     fold mul_factor m' m
 
-  let subst id p m =
-    fold begin fun f e res ->
-      Pol.mul (Pol.pow e (Fac.subst id p f)) res
-    end m (Pol.const 1.)
-
 end
 
-module MkPoly(Fac: Factor)(Mon: Monom with type factor = Fac.t)
-: Poly with type factor = Fac.t and type monom = Mon.t
+module MkPoly(Mon: Monom)
+: Poly with type monom = Mon.t
 = struct
 
   (* Polynomials are represented as maps from monomials
@@ -110,13 +93,14 @@ module MkPoly(Fac: Factor)(Mon: Monom with type factor = Fac.t)
   *)
 
   module M = Map.Make(Mon)
-  type t = float M.t and factor = Fac.t and monom = Mon.t
+  type monom = Mon.t
+  type t = float M.t
 
   let zero = M.empty
   let const k = M.add Mon.one k zero
   let compare = (M.compare compare: t -> t -> int)
   let fold = M.fold
-  let of_factor f = M.add (Mon.of_factor f) 1. zero
+  let of_monom m = M.add m 1. zero
 
   let get_coeff m pol =
     try M.find m pol with Not_found -> 0.
@@ -147,34 +131,35 @@ module MkPoly(Fac: Factor)(Mon: Monom with type factor = Fac.t)
     if n = 0 then const 1. else
     mul pol (pow (n-1) pol)
 
-  let subst id p p' =
-    let p: Mon.poly = Obj.magic p in
-    fold begin fun m k res ->
-
-      (* Because we will always be using this module in
-         the recursive definition below, the Obj.magic
-         is safe.  [Mon.poly] is the same type as [t]
-         at runtime.
-      *)
-      add_scale k (Obj.magic (Mon.subst id p m): t) res
-
-    end p' zero
-
   let zero () = zero
 
 end
 
 module
   rec Poly
-  : Poly with type factor = Factor.t
-    and type monom = Monom.t
-  = MkPoly(Factor)(Monom)
+  : Poly with type monom = Monom.t
+  = MkPoly(Monom)
 
   and Monom
   : Monom with type factor = Factor.t
-    and type poly = Poly.t
-  = MkMonom(Poly)(Factor)
+  = MkMonom(Factor)
 
   and Factor
   : Factor with type poly = Poly.t
   = MkFactor(Poly)
+
+
+let rec factor_subst id p = function
+  | Factor.Var v when v = id -> p
+  | Factor.Max p' -> Poly.of_monom (Monom.of_factor (Factor.Max (poly_subst id p p')))
+  | f -> Poly.of_monom (Monom.of_factor f)
+
+and monom_subst id p m =
+  Monom.fold begin fun f e res ->
+    Poly.mul (Poly.pow e (factor_subst id p f)) res
+  end m (Poly.const 1.)
+
+and poly_subst id p p' =
+  Poly.fold begin fun m k res ->
+    Poly.add_scale k (monom_subst id p m) res
+  end p' (Poly.zero ())
