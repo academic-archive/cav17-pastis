47ba8b0fe2199427e9bb9da4e8eee7327357a5a2
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2016-05-19 10:31:33 -0400

    test substitution in max factors

--- source/Polynom_Test.ml
+++ source/Polynom_Test.ml
@@ -4,12 +4,13 @@ let _ =
   let zero = Poly.zero () in
   let x15 = Poly.of_monom (Monom.of_factor (Factor.Var "x") 15) 1. in
   let z = Poly.of_monom (Monom.of_factor (Factor.Var "z") 1) 1. in
+  let maxz = Poly.of_monom (Monom.of_factor (Factor.Max z) 3) (-0.5) in
   let ym1 =
-    Poly.add (Poly.of_monom (Monom.of_factor (Factor.Var "y") 1) 1.)
-             (Poly.of_monom (Monom.of_factor (Factor.Max z) 1) (-0.5)) in
+    Poly.add (Poly.of_monom (Monom.of_factor (Factor.Var "y") 1) 1.) maxz in
   begin
     Format.printf "::@.%a@." Poly.print zero;
     Format.printf "::@.%a@." Poly.print x15;
     Format.printf "::@.%a@." Poly.print ym1;
+    Format.printf "::@.%a@." Poly.print (poly_subst "z" (Poly.const 42.) maxz);
     Format.printf "::@.%a@." Poly.print (poly_subst "x" ym1 x15);
   end
