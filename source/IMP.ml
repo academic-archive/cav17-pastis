d7fadc47503fc4dd5e2c77077afb32813184259e
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2016-06-01 10:14:11 -0400

    spacing modifications

--- source/IMP.ml
+++ source/IMP.ml
@@ -5,20 +5,18 @@ include IMP_Types
 
 let parse_file f =
   let ic =
-    try open_in f with
-    Sys_error _ ->
+    try open_in f
+    with Sys_error _ ->
       Format.eprintf "cannot open file '%s'@." f;
       raise Utils.Error
   in
   let lexbuf = Lexing.from_channel ic in
   lexbuf.Lexing.lex_curr_p <-
-    { lexbuf.Lexing.lex_curr_p with Lexing.
-      pos_fname = f
-    };
+    { lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = f };
   try
     let l = IMP_Grammar.file IMP_Lexer.token lexbuf in
     List.map (fun f ->
-      { f with fun_focus = List.map Focus.interpret f.fun_focus } ) l
+      { f with fun_focus = List.map Focus.interpret f.fun_focus }) l
   with Parsing.Parse_error ->
     let startp = Lexing.lexeme_start_p lexbuf in
     Format.eprintf "%s:%i:%i: syntax error near '%s'@."
