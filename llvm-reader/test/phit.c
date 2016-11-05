bdff05dcac24ca5b5b4dd50c98222e3a84162ab2
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2016-11-05 12:46:53 -0400

    remove debug tests

--- llvm-reader/test/phit.c
+++
@@ -1,6 +0,0 @@
-void test(int n, int c) {
-  while (n > 0 || c <= 1) {
-    n = n - 1;
-  }
-}
-
--- llvm-reader/test/phitf.c
+++
@@ -1,6 +0,0 @@
-void test(int n, int c, int d) {
-  while ((n > 0 && c <= 1) && d > 2) {
-    n = n - 1;
-  }
-}
-
