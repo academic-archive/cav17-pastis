4794a731413c7646901404057e0855c26eb32cb3
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2016-12-14 14:58:46 -0500

    whitespace nits for my small screen

--- source/Coqgen.ml
+++ source/Coqgen.ml
@@ -4,41 +4,82 @@ open Presburger
 
 (* todo: "dump" is probably not quite the right word, what should it be called? *)
 
-let mkvarname funname x = "ID"^funname^"_"^x
+let mkvarname funname x = "ID" ^ funname ^ "_" ^ x
 
 (* Assumes that we have a state s in the context. *)
 let rec dump_expr varname fmt  = function
-    ERandom -> Format.fprintf fmt "sorry, I don't know how to handle random."
-  | EVar x  -> Format.fprintf fmt "(EVar %s)" (varname x)
-  | ENum n  -> Format.fprintf fmt "(ENum %d)" n
-  | EAdd (e1, e2) -> Format.fprintf fmt "(EAdd %a@ %a)" (dump_expr varname) e1 (dump_expr varname) e2
-  | ESub (e1, e2) -> Format.fprintf fmt "(ESub %a@ %a)" (dump_expr varname) e1 (dump_expr varname) e2
-  | EMul (e1, e2) -> Format.fprintf fmt "(EMul %a@ %a)" (dump_expr varname) e1 (dump_expr varname) e2
+  | ERandom ->
+    Utils._TODO "coqgen random expressions"
+  | EVar x ->
+    Format.fprintf fmt "(EVar %s)" (varname x)
+  | ENum n ->
+    Format.fprintf fmt "(ENum %d)" n
+  | EAdd (e1, e2) ->
+     Format.fprintf fmt "(EAdd %a@ %a)"
+       (dump_expr varname) e1
+       (dump_expr varname) e2
+  | ESub (e1, e2) ->
+    Format.fprintf fmt "(ESub %a@ %a)"
+      (dump_expr varname) e1
+      (dump_expr varname) e2
+  | EMul (e1, e2) ->
+    Format.fprintf fmt "(EMul %a@ %a)"
+      (dump_expr varname) e1
+      (dump_expr varname) e2
 
 (* Assumes that we have a state s in the context. *)
 let rec dump_logic varname fmt = function
-    LTrue -> Format.fprintf fmt "True"
-  | LFalse -> Format.fprintf fmt "False"
-  | LRandom -> Format.fprintf fmt "sorry, I don't know how to handle random."
-  | LCmp(e1,cmp,e2) -> Format.fprintf fmt "((eval %a@ s) %s@ (eval %a@ s))%%Z"
-				      (dump_expr varname) e1
-				      (match cmp with Le -> "<=" | Lt -> "<" | Ge -> ">=" | Gt -> ">" | Eq -> "=" | Ne -> "<>")
-				      (dump_expr varname) e2
-  | LAnd(l1,l2) -> Format.fprintf fmt "(%a@ /\\ %a)" (dump_logic varname) l1 (dump_logic varname) l2
-  | LOr(l1,l2)  -> Format.fprintf fmt "(%a@ \\/ %a)" (dump_logic varname) l1 (dump_logic varname) l2
-  | LNot l -> Format.fprintf fmt "(~ %a)" (dump_logic varname) l
+  | LTrue ->
+    Format.fprintf fmt "True"
+  | LFalse ->
+    Format.fprintf fmt "False"
+  | LRandom ->
+    Utils._TODO "coqgen random logic"
+  | LCmp (e1,cmp,e2) ->
+    let cmp = match cmp with
+      | Le -> "<="
+      | Lt -> "<"
+      | Ge -> ">="
+      | Gt -> ">"
+      | Eq -> "="
+      | Ne -> "<>"
+    in
+    Format.fprintf fmt "((eval %a@ s) %s@ (eval %a@ s))%%Z"
+      (dump_expr varname) e1
+      cmp
+      (dump_expr varname) e2
+  | LAnd (l1,l2) ->
+    Format.fprintf fmt "(%a@ /\\ %a)"
+      (dump_logic varname) l1
+      (dump_logic varname) l2
+  | LOr (l1,l2) ->
+    Format.fprintf fmt "(%a@ \\/ %a)"
+      (dump_logic varname) l1
+      (dump_logic varname) l2
+  | LNot l ->
+     Format.fprintf fmt "(~ %a)"
+       (dump_logic varname) l
 
 let dump_action varname fmt = function
-  | ANone -> Format.fprintf fmt "ANone"
-  | AWeaken -> Format.fprintf fmt "AWeaken"
-  | AGuard a -> Format.fprintf fmt "(AGuard@ (fun s => %a))" (dump_logic varname) a
-  | AAssign(x, e) -> Format.fprintf fmt "(AAssign@ %s@ %a)" (varname x) (dump_expr varname) e
-  | ACall  (xs,y,es) -> Format.fprintf fmt "TODO: implement ACall"
+  | ANone ->
+    Format.fprintf fmt "ANone"
+  | AWeaken ->
+    Format.fprintf fmt "AWeaken"
+  | AGuard a ->
+    Format.fprintf fmt "(AGuard@ (fun s => %a))"
+      (dump_logic varname) a
+  | AAssign (x, e) ->
+    Format.fprintf fmt "(AAssign@ %s@ %a)"
+      (varname x)
+      (dump_expr varname) e
+  | ACall (xs,y,es) ->
+    Utils._TODO "coqgen calls"
 
 let dump_edges' varname fmt s es =
   List.iter (fun (a,s') ->
-	     Format.fprintf fmt "@[<hov>(%d,@,%a,@,%d)@]::@," s (dump_action varname) a s')
-	    es
+    Format.fprintf fmt "@[<hov>(%d,@,%a,@,%d)@]::@,"
+      s (dump_action varname) a s'
+  ) es
 
 let dump_edges varname fmt es =
   Format.fprintf fmt "@[<hov>";
@@ -56,33 +97,43 @@ let dump_func fmt i f =
   Format.fprintf fmt "\n\nOpaque Zplus.\nOpaque Zmult.@]@.@.";
 
   Format.fprintf fmt "@[<v>";
-  List.iteri (fun i x -> Format.fprintf fmt "Notation %s := %d.@," (varname x) i) f.fun_vars;
+  List.iteri (fun i x ->
+    Format.fprintf fmt "Notation %s := %d.@," (varname x) i
+  ) f.fun_vars;
   Format.fprintf fmt "@]";
 
-  Format.fprintf fmt "@[<v>Definition func%d : graph :=@," i;
-  Format.fprintf fmt "  {| g_start := %d;@," f.fun_body.g_start;
-  Format.fprintf fmt "     g_end := %d;@," f.fun_body.g_end;
-  Format.fprintf fmt "     g_edges := %a@," (dump_edges varname) f.fun_body.g_edges;
-  Format.fprintf fmt "  |}.@]"
+  Format.fprintf fmt
+    "@[<v>Definition func%d : graph :=@,\
+       {| g_start := %d;@,\
+          g_end := %d;@,\
+          g_edges := %a@,\
+       |}.@]"
+    i f.fun_body.g_start f.fun_body.g_end
+    (dump_edges varname) f.fun_body.g_edges
 
 let dump_ai_func_bounds print_bound fmt bounds =
   Format.fprintf fmt "@[<v>";
-  Array.iteri (fun i v -> Format.fprintf fmt "| %d => %a@," i print_bound v)
-	      bounds;
+  Array.iteri (fun i v ->
+    Format.fprintf fmt "| %d => %a@," i print_bound v
+  ) bounds;
   Format.fprintf fmt "| _ => False@,";
   Format.fprintf fmt "@]"
 
 let dump_ai_bounds print_bound fmt bounds =
+  (* TODO: print the correct function name *)
   Format.fprintf fmt "@[<v>";
   Hashtbl.iter (fun funname b ->
-		(*todo: print the correct function name*)
-	        Format.fprintf fmt "Definition func0_bounds (p : node) (s : state) := @,";
-		Format.fprintf fmt "  match p with@,";
-		Format.fprintf fmt "    @[<v4>%a@]@," (dump_ai_func_bounds (print_bound (mkvarname funname))) b)
-     	                                              bounds;
+    Format.fprintf fmt "Definition func0_bounds (p : node) (s : state) := @,";
+    Format.fprintf fmt "  match p with@,";
+    Format.fprintf fmt "    @[<v4>%a@]@,"
+      (dump_ai_func_bounds (print_bound (mkvarname funname))) b;
+  ) bounds;
   Format.fprintf fmt "  end.@,";
-  Format.fprintf fmt "Theorem func0_bounds_corrects : forall s p' s', steps (g_start func0) s func0 p' s' -> func0_bounds p' s'.@,";
-  Format.fprintf fmt "Proof. prove_ai_bounds_correct. Qed.";
+  Format.fprintf fmt
+    "Theorem func0_bounds_corrects : \
+    forall s p' s', steps (g_start func0) s func0 p' s' -> \
+    func0_bounds p' s'.@,\
+    Proof. prove_ai_bounds_correct. Qed.";
   Format.fprintf fmt "@]"
 
 let dump fs print_bound ai_bounds _annot =
