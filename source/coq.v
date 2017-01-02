28c36fc7f2cd1ea2c2d06df7aafbac1680aecc02
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2017-01-02 12:36:06 -0500

    use new library in example

--- source/coq.v
+++ source/coq.v
@@ -1,18 +1,5 @@
 Add LoadPath "coq".
-Require Import List.
-Require Import Arith.
-Require Import ZArith.
-Require Import QArith.
-Require Import CFG.
-Require Import Rewr.
-Close Scope Z.
-Close Scope Q.
-Opaque Zminus.
-Opaque Zplus.
-Opaque Zmult.
-Import ListNotations.
-
-Coercion inject_Z: Z >-> Q.
+Require Import Pasta.
 
 Notation IDfunc0_t := 0.
 Notation IDfunc0_z := 1.
@@ -50,7 +37,8 @@ Definition func0_bounds (p : node) (s : state) :=
   end.
 Theorem func0_bounds_corrects:
   forall s p' s', steps (g_start func0) s (g_edges func0) p' s' -> func0_bounds p' s'.
-Proof. prove_ai_bounds_correct. Qed.
+Proof. check_ai. Qed.
+
 
 Definition func0_annots (p : node) (s : state) := 
   match p with
@@ -89,103 +77,9 @@ Definition func0_hints (p : node) (s : state) :=
     | _ => []
   end.
 
-Require Import Psatz.
-
-Definition rewrite_conclusion (a: rewrite_ast) :=
-  (ineq_ge0 (snd (interpret a)) >= 0)%Z.
-Definition rewrite_assumptions (a: rewrite_ast) :=
-  Forall ineq_prop (fst (interpret a)).
-
-Lemma use_hints:
-  forall hints,
-    Forall rewrite_assumptions hints ->
-    Forall rewrite_conclusion hints.
-Proof.
-  induction hints.
-  + tauto.
-  + simpl; intuition.
-    eapply ineq_ge0_semantics.
-    reflexivity.
-    case_eq (interpret a); intros.
-    eapply rewrite_ast_semantics.
-    eassumption.
-    unfold rewrite_assumptions in H0.
-    rewrite H2 in H0.
-    assumption.
-Qed.
-
-Ltac simplify_max0 :=
-
-  (* Uniformize contents of max0 terms. *)
-  repeat match goal with
-  | [ |- context[ max0 ?a ] ] =>
-    match goal with
-    | [ |- context[ max0 ?b ] ] =>
-      let H := fresh "H" in
-      (assert (H: a = b) by reflexivity; fail 1) ||
-      (assert (H: a = b) by ring; rewrite H; clear H)
-    end
-  end;
-
-  (* Reduce max0 when the argument is a constant. *)
-  repeat match goal with
-  | [ |- context[ max0 ?x ] ] => set (max0 x)
-  end;
-  repeat match goal with
-  | [ m := max0 ?x |- _ ] => revert m; ring_simplify x
-  end;
-  repeat match goal with
-  | [ |- context[ max0 (Zpos ?x) ] ] =>
-    change (max0 (Zpos x)) with (Zpos x)
-  | [ |- context[ max0 0 ] ] =>
-    change (max0 0) with 0
-  | [ |- context[ max0 (Zneg ?x) ] ] =>
-    change (max0 (Zneg x)) with 0
-  end.
-
 
 Theorem foo:
   forall s p' s',
     steps (g_start func0) s (g_edges func0) p' s' ->
     (func0_annots (g_start func0) s >= func0_annots p' s')%Q.
-Proof.
-  intros until 0.
-  apply reachable_ind with
-  (P := fun p' s' => (func0_annots (g_start func0) s >= func0_annots p' s')%Q);
-    auto with qarith.
-
-  repeat apply conj;
-    
-  try match goal with
-  | [ |- context[ AAssign _ _ ] ] =>
-    inversion_clear 2;
-    unfold update;
-    apply Qle_trans, Z.eq_le_incl;
-    simpl; simplify_max0;
-    ring (* Hopefully that does it... *)
-
-  | [ |- context[ AWeaken ] ] =>
-    intros until 1;
-    refine (_ (func0_bounds_corrects _ _ _ REACHABLE));
-    intro; inversion 1; subst;
-    clear REACHABLE; apply Qle_trans;
-    match goal with
-    | [ |- (_ <= _ ?n ?s)%Q ] =>
-      refine (_ (use_hints (func0_hints n s) _));
-      unfold Qle, rewrite_conclusion, rewrite_assumptions;
-      simpl in *; simplify_max0; try lia
-    end
-                     
-  | [ |- context[ AGuard _ ] ] =>
-    inversion_clear 2;
-    now apply Qle_trans, Qle_refl
-
-  | [ |- context[ ANone ] ] =>
-    inversion_clear 2;
-    now apply Qle_trans, Qle_refl
-
-  | [ |- True ] => constructor
-
-  end.
-
-Qed.
+Proof. check_lp func0_bounds_corrects func0_annots func0_hints. Qed.
